2d9c2d54c8ae6ad56cd331209c201aa5efc59a08
Joseph Montoya <j.jhoel@outlook.es>
2026-07-10 21:00:56 -0500

    feat: mejorando vistas de cliente

--- soporte/forms.py
+++ soporte/forms.py
@@ -8,7 +8,7 @@ class TicketForm(forms.ModelForm):
         model = Ticket
         fields = ['categoria', 'asunto', 'descripcion']
         widgets = {
-            'categoria': forms.Select(attrs={'class': 'select select-bordered w-full'}),
-            'asunto': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
-            'descripcion': forms.Textarea(attrs={'class': 'textarea textarea-bordered w-full', 'rows': 4}),
+            'categoria': forms.Select(attrs={'class': 'select select-bordered bg-base-200 w-full'}),
+            'asunto': forms.TextInput(attrs={'class': 'input input-bordered bg-base-200 w-full'}),
+            'descripcion': forms.Textarea(attrs={'class': 'textarea textarea-bordered bg-base-200 w-full', 'rows': 4}),
         }
