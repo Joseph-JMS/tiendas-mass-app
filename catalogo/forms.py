c046936cf41a25005582ac072f3d44c3cfc5ec75
Joseph Montoya <j.jhoel@outlook.es>
2026-07-19 19:48:36 -0500

    feat: mejorando paginas de admin

--- catalogo/forms.py
+++ catalogo/forms.py
@@ -11,13 +11,13 @@ class ProductoForm(forms.ModelForm):
             'stock', 'stock_minimo', 'fecha_vencimiento', 'imagen', 'activo'
         ]
         widgets = {
-            'categoria': forms.Select(attrs={'class': 'select select-bordered w-full'}),
-            'nombre': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
-            'descripcion': forms.Textarea(attrs={'class': 'textarea textarea-bordered w-full'}),
-            'precio': forms.NumberInput(attrs={'class': 'input input-bordered w-full'}),
-            'stock': forms.NumberInput(attrs={'class': 'input input-bordered w-full'}),
-            'stock_minimo': forms.NumberInput(attrs={'class': 'input input-bordered w-full'}),
-            'fecha_vencimiento': forms.DateInput(attrs={'class': 'input input-bordered w-full', 'type': 'date'}),
-            'imagen': forms.ClearableFileInput(attrs={'class': 'file-input file-input-bordered w-full'}),
-            'activo': forms.CheckboxInput(attrs={'class': 'checkbox'}),
+            'categoria': forms.Select(attrs={'class': 'select select-bordered bg-base-200 w-full'}),
+            'nombre': forms.TextInput(attrs={'class': 'input input-bordered bg-base-200 w-full'}),
+            'descripcion': forms.Textarea(attrs={'class': 'textarea textarea-bordered bg-base-200 w-full', 'rows': 3}),
+            'precio': forms.NumberInput(attrs={'class': 'input input-bordered bg-base-200 w-full'}),
+            'stock': forms.NumberInput(attrs={'class': 'input input-bordered bg-base-200 w-full'}),
+            'stock_minimo': forms.NumberInput(attrs={'class': 'input input-bordered bg-base-200 w-full'}),
+            'fecha_vencimiento': forms.DateInput(attrs={'class': 'input input-bordered bg-base-200 w-full', 'type': 'date'}),
+            'imagen': forms.ClearableFileInput(attrs={'class': 'file-input file-input-bordered bg-base-200 w-full'}),
+            'activo': forms.CheckboxInput(attrs={'class': 'checkbox checkbox-primary'}),
         }
